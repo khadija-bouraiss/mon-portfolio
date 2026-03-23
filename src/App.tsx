1ad61964ac943a45a7726a619caed20a9dc93998
BOURAISS <khadijabouraiss11@gmail.com>
2026-03-24 00:22:23 +0100

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <div className="p-6 text-blue-500">
+      <h1 className="text-2xl font-bold">Tailwind fonctionne !</h1>
+    </div>
+  )
+}
+export default App
